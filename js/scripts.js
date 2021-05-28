4be95e2b7f01c198b2c38f85b038319b31c2a42a
Archie <chientrisha@gmail.com>
2021-05-28 14:55:58 +0800

    technical exam, fixed issues, scss, fixed search

--- js/scripts.js
+++ js/scripts.js
@@ -92,43 +92,37 @@ $(function () {
 			$('#search__results--display').html('<img src="img/ajax-loader.gif" alt="loading" />');			
 		
 			var texts = "";
-			$.ajax({
-				"url": "https://itunes.apple.com/search",
-				"dataType": "jsonp",
-				"processData": false, 
-				"contentType": false, 
-				"cache": false,
-				"data": {
-					"term": encodeURI(keyword),
-					"media": "movie",
-					"attribute": "featureFilmTerm"
-				},
-				"error": function (jqXHR, textStatus, message) {
-					console.log(message);
-				},
-				"success": function (data, textStatus, jqXHR) {
-					console.log(data);
-					data.results.foreach(function (val, index, array) {
-						$('#search__results--display').html('');	
+			var i = 0;
+			$.getJSON("https://itunes.apple.com/search?media=movie&attribute=featureFilmTerm&term="+encodeURI(keyword), function (data) {		
+				var count = data.resultCount;
+				texts = '<div class="container"> <div class="row text-center">';
+					if(count>0) {
+						$.each(data.results, function(i, field){
+							//console.log(i.results);
+							//alert( i + ": " + field );
+							//alert(field.artistName);
+							var artist = field.artistName;
+							var title = field.trackName;
+							var video = field.previewUrl;
+							var price = field.trackPrice;
+							var rent = field.trackRentalPrice;
+							var cat = field.primaryGenreName;
+							var summary = field.longDescription;
+							var rdate = field.releaseDate;
+							var img = field.artworkUrl100;
+							var bigImg = img.replace('100x100bb.jpg','300x300bb.jpg');
 						
-						var artist = val.artistName;
-						var title = val.trackName;
-						var video = val.previewUrl;
-						var price = val.trackPrice;
-						var rent = val.trackRentalPrice;
-						var cat = val.primaryGenreName;
-						var summary = val.longDescription;
-						var rdate = val.releaseDate;
-						var img = val.artworkUrl100;
-						var bigImg = img.replace('100x100bb.jpg','300x300bb.jpg');
-						
-						texts = '<div class="container"> <div class="row text-center"><div class="col-4">';
-						texts = texts + '<div class="img__container"><img src="'+bigImg+'" alt="" class="img__image" /><div class="img__middle"><div class="img__text"><a href="#movie_details" data-mdb-toggle="modal"  data-mdb-dismiss="modal" data-mdb-target="#movie_details" data-title="'+title.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-image="'+bigImg+'" data-summary="'+summary.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-category="'+cat+'" data-rdate="'+rdate+'" data-rent="'+rent+'" data-price="'+price+'" data-artist="'+artist+'" data-trailer="'+video+'">Details</a></div></div></div><h4>'+title+'</h4>';
-						texts = texts + '</div></div></div>';
-						
-						$('#search__results--display').append(texts);
-					});
-				}
+							texts = texts + '<div class="col-md-2 col-2"><div class="img__container"><img src="'+bigImg+'" alt="" class="w-100 img__image" /><div class="img__middle"><button class="btn btn--details" data-mdb-toggle="modal" data-mdb-dismiss="modal" data-mdb-target="#movie_details" data-title="'+title.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-image="'+bigImg+'" data-summary="'+summary.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-category="'+cat+'" data-rdate="'+rdate+'" data-rent="'+rent+'" data-price="'+price+'" data-artist="'+artist+'" data-trailer="'+video+'">Details</button></div></div></div>';
+							
+						});				
+					}else {
+						texts = 'No movies found.';
+					}	
+					
+
+					texts = texts + '</div></div>';
+				$('#search__results--display').html('');			
+				$('#search__results--display').append(texts);
 			});
 		}
 	});	
@@ -154,7 +148,6 @@ $(function () {
 															
 			body = '';
 			body = body + '<div class="img__container"><img src="'+bigImg+'" alt="" class="w-100 img__image" /><div class="img__middle"><button class="btn btn--details" data-mdb-toggle="modal" data-mdb-dismiss="modal" data-mdb-target="#movie_details" data-title="'+title.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-image="'+bigImg+'" data-summary="'+summary.replace(/[0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi,'')+'" data-category="'+cat+'" data-rdate="'+rdate+'" data-rent="'+rent+'" data-price="'+price+'" data-artist="'+artist+'" data-trailer="'+video+'">Details</button></div></div>';
-			body = body + '';
 			
 			$('.bxslider').append(body);
 
